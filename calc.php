c023dd119952546a33b25680c5a29ea847300221
Vadim Sitnikof <prognozman@mail.com>
2022-01-16 16:51:14 +0500

    version 3 set logic

--- calc.php
+++ calc.php
@@ -1,5 +1,39 @@
 <?php
 
+header("Content-Type: application/json");
+$data = json_decode(file_get_contents("php://input"), true);
+$date = date_parse_from_format("j.n.Y", $data['startDate']);
+$depAmount = $data['sum'];
+$percent = $data['percent'];
+$replAmount = $data['sumAdd'];
+$curMonth = $date["month"];
+const daysY = 365;
+const convert = 100;
+$daysInMonth = array(
+    1 => 31,
+    2 => 28,
+    3 => 31,
+    4 => 30,
+    5 => 31,
+    6 => 30,
+    7 => 31,
+    8 => 31,
+    9 => 30,
+    10 => 31,
+    11 => 30,
+    12 => 31
+);
+
+for ($i = 0; $i < $data["term"]; $i++) {
+    $depAmount += (($depAmount + $replAmount) * $daysInMonth[$curMonth] * ($percent / daysY)) / convert;
+    if($curMonth < 12) {
+        $curMonth++;
+    } else {
+        $curMonth = 1;
+    }
+}
+
+echo $depAmount;
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- script.js
+++ script.js
@@ -3,14 +3,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
 function setForm() {
-var chbox;
-var form;
-form = document.getElementById('replAmount');
-chbox=document.getElementById('monthlyReplenishment');
-	if (chbox.checked) {
-		form.style.display = "block";
-	}
-	else {
-		form.style.display = "none";
-	}
+    var chbox;
+    var form;
+    form = document.getElementById('replAmount');
+    chbox = document.getElementById('monthlyReplenishment');
+    if (chbox.checked) {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
 }
+document.querySelector('.calculate').onclick = calculate;
+var result = document.querySelector('#result');
+function calculate() {
+    let starDate = document.querySelector('#startDate').value;
+    let depositAmount = document.querySelector('#depositAmount').value;
+    let percent = document.querySelector('#percent').value;
+    let replAmount = document.querySelector('#replAmount').value;
+    let depositTerm = document.querySelector('#depositTerm').value;
+    let select = document.querySelector('#time');
+    var xhr = new XMLHttpRequest();
+    if ($.isEmptyObject(replAmount)) {
+        replAmount = 0;
+    }
+    
+    xhr.open('POST', 'calc.php');
+    xhr.setRequestHeader('Content-Type', 'Content-Type: application/json');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            result.textContent = this.responseText;
+        }
+    };
+    console.log(replAmount)
+    var data = JSON.stringify({
+        "startDate": starDate, // дата открытия вклада
+        "sum": depositAmount, // сумма вклада
+        "term": depositTerm, // срок вклада в месяцах
+        "percent": percent, // процентная ставка, % годовых
+        "sumAdd": replAmount // сумма ежемесячного пополнения вклада
+    });
+    xhr.send(data);
+    return false;
+}
+function validation(val){
+    
+}
+
